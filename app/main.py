3b42d7a03a132b21b73c485a59c900fa487ff882
Little Coder <kimriana@yahoo.com>
2025-08-10 22:32:29 +0700

    feat(talent-growth): setup FastAPI project structure

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from . import models, database, routes
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI(
+    title="Task Management API",
+    description="API for managing tasks with CRUD, filtering, and sorting, built with **FastAPI** and **MySQL**.",
+    version="1.0.0"
+)
+
+app.include_router(routes.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Task Management API"}
